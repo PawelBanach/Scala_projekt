ab649b2010169b0eec394647bb96f730fb924507
PawelBanach <pawelbanachweb@gmail.com>
2016-01-16 10:09:34 +0100

    Update Application.scala
    Jeszcze dodane komentarze

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,6 +12,11 @@ class Application extends Controller {
 	//https://www.playframework.com/documentation/2.4.0-M2/api/scala/index.html#play.api.db.slick.DBAction$ <= świetnie wytłumaczone
 	//https://github.com/playframework/play-slick/wiki/Usage <= tu też
 	//Use the DBAction when you want to use the default settings in your controller.
+	//index powstaje według template'a, DBAction używamy specjalnie jeśli chcemy miec domyślne ustawienia, Ok to taki
+  	// router który zwraca nam to co zostaje zbudowane, w pliku conf/routes definiujemy akcje jakie chcemy żeby nasza
+  	// aplikacja miała, jeżeli chodzi o argumenty, pierwszy to message, drugi to lista(clasa z metodą która zwraca
+	 // liste) a trzeci to Form, czyli taka
+	 // specjalna struktura danych do formularzy https://www.playframework.com/documentation/2.0/ScalaForms
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
